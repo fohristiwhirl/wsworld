501da473f278ef1f22d832c5582fc0fcfacbba6e
fohristiwhirl <fohristiwhirl@users.noreply.github.com>
2017-02-27 04:41:32 +0000

    make the Client a single object. Dunno if my JS style is sane.

--- webpage.go
+++ webpage.go
@@ -71,241 +71,257 @@ Total draws: <span id="total_draws">0</span>
 
 {{.ImageLoaders}}
 
-var WIDTH = {{.Width}};
-var HEIGHT = {{.Height}};
+document.querySelector("canvas").width = {{.Width}};
+document.querySelector("canvas").height = {{.Height}};
 
-var ws_frames = 0;
-var total_draws = 0;
+function WsWorldClient() {
 
-var second_last_frame_time = Date.now() - 16;
-var last_frame_time = Date.now();
+    var that = this;
 
-var virtue = document.querySelector("canvas").getContext("2d");
-document.querySelector("canvas").width = WIDTH;
-document.querySelector("canvas").height = HEIGHT;
+    var WIDTH = {{.Width}};
+    var HEIGHT = {{.Height}};
+    var channel_max = 8;
+    var virtue = document.querySelector("canvas").getContext("2d");
 
-var channel_max = 8;
-var audiochannels = [];
-var all_things = [];
+    that.ws_frames = 0;
+    that.total_draws = 0;
+    that.second_last_frame_time = Date.now() - 16;
+    that.last_frame_time = Date.now();
+    that.all_things = [];
+    that.audiochannels = [];
 
+    that.parse_point_or_sprite = function (blob) {
 
-function parse_point_or_sprite(blob) {
+        var elements = blob.split(":");
 
-    var elements = blob.split(":");
+        var thing = {};
 
-    var thing = {};
+        thing.type = elements[0];
 
-    thing.type = elements[0];
+        if (thing.type === "p") {
+            thing.colour = elements[1];
+        } else if (thing.type === "s") {
+            thing.varname = elements[1];
+        }
 
-    if (thing.type === "p") {
+        thing.x = parseFloat(elements[2]);
+        thing.y = parseFloat(elements[3]);
+        thing.speedx = parseFloat(elements[4]);
+        thing.speedy = parseFloat(elements[5]);
+
+        that.all_things.push(thing);
+    };
+
+    that.parse_line = function (blob) {
+
+        var elements = blob.split(":");
+
+        var thing = {};
+
+        thing.type = elements[0];
         thing.colour = elements[1];
-    } else if (thing.type === "s") {
-        thing.varname = elements[1];
-    }
+        thing.x1 = parseFloat(elements[2]);
+        thing.y1 = parseFloat(elements[3]);
+        thing.x2 = parseFloat(elements[4]);
+        thing.y2 = parseFloat(elements[5]);
+        thing.speedx = parseFloat(elements[6]);
+        thing.speedy = parseFloat(elements[7]);
 
-    thing.x = parseFloat(elements[2]);
-    thing.y = parseFloat(elements[3]);
-    thing.speedx = parseFloat(elements[4]);
-    thing.speedy = parseFloat(elements[5]);
+        that.all_things.push(thing);
+    };
 
-    all_things.push(thing);
-}
+    that.draw_point = function (p, time_offset) {
+        var x = Math.floor(p.x + p.speedx * time_offset / 1000);
+        var y = Math.floor(p.y + p.speedy * time_offset / 1000);
+        virtue.fillStyle = p.colour;
+        virtue.fillRect(x, y, 1, 1);
+    };
 
-function parse_line(blob) {
+    that.draw_sprite = function (sp, time_offset) {
+        var x = sp.x + sp.speedx * time_offset / 1000;
+        var y = sp.y + sp.speedy * time_offset / 1000;
+        virtue.drawImage(window[sp.varname], x - window[sp.varname].width / 2, y - window[sp.varname].height / 2);
+    };
 
-    var elements = blob.split(":");
+    that.draw_line = function (li, time_offset) {
+        var x1 = li.x1 + li.speedx * time_offset / 1000;
+        var y1 = li.y1 + li.speedy * time_offset / 1000;
+        var x2 = li.x2 + li.speedx * time_offset / 1000;
+        var y2 = li.y2 + li.speedy * time_offset / 1000;
 
-    var thing = {};
+        virtue.strokeStyle = li.colour;
+        virtue.beginPath();
+        virtue.moveTo(x1, y1);
+        virtue.lineTo(x2, y2);
+        virtue.stroke();
+    };
 
-    thing.type = elements[0];
-    thing.colour = elements[1];
-    thing.x1 = parseFloat(elements[2]);
-    thing.y1 = parseFloat(elements[3]);
-    thing.x2 = parseFloat(elements[4]);
-    thing.y2 = parseFloat(elements[5]);
-    thing.speedx = parseFloat(elements[6]);
-    thing.speedy = parseFloat(elements[7]);
+    that.draw = function () {
 
-    all_things.push(thing);
-}
+        // As a relatively simple way of dealing with arbitrary timings of incoming data, we
+        // always try to draw the object "where it is now" taking into account how long it's
+        // been since we received info about it. This is done with this "time_offset" var.
 
-function draw_point(p, time_offset) {
-    var x = Math.floor(p.x + p.speedx * time_offset / 1000);
-    var y = Math.floor(p.y + p.speedy * time_offset / 1000);
-    virtue.fillStyle = p.colour;
-    virtue.fillRect(x, y, 1, 1);
-}
+        var time_offset = Date.now() - that.last_frame_time;
 
-function draw_sprite(sp, time_offset) {
-    var x = sp.x + sp.speedx * time_offset / 1000;
-    var y = sp.y + sp.speedy * time_offset / 1000;
-    virtue.drawImage(window[sp.varname], x - window[sp.varname].width / 2, y - window[sp.varname].height / 2);
-}
+        virtue.clearRect(0, 0, WIDTH, HEIGHT);     // The best way to clear the canvas??
 
-function draw_line(li, time_offset) {
-    var x1 = li.x1 + li.speedx * time_offset / 1000;
-    var y1 = li.y1 + li.speedy * time_offset / 1000;
-    var x2 = li.x2 + li.speedx * time_offset / 1000;
-    var y2 = li.y2 + li.speedy * time_offset / 1000;
+        var all_things = that.all_things;
+        var len = all_things.length;
+        var n;
 
-    virtue.strokeStyle = li.colour;
-    virtue.beginPath();
-    virtue.moveTo(x1, y1);
-    virtue.lineTo(x2, y2);
-    virtue.stroke();
-}
+        // Cache the functions to cut down on indirection (I think).
+        // Might offer a speedup? Who knows...
 
-function draw() {
+        var draw_point = that.draw_point
+        var draw_sprite = that.draw_sprite
+        var draw_line = that.draw_line
 
-    // As a relatively simple way of dealing with arbitrary timings of incoming data, we
-    // always try to draw the object "where it is now" taking into account how long it's
-    // been since we received info about it. This is done with this "time_offset" var.
+        for (n = 0; n < len; n += 1) {
 
-    var time_offset = Date.now() - last_frame_time;
-
-    virtue.clearRect(0, 0, WIDTH, HEIGHT);     // The best way to clear the canvas??
-
-    var len = all_things.length;
-    var n;
-
-    for (n = 0; n < len; n += 1) {
-
-        switch (all_things[n].type) {
-        case "p":
-            draw_point(all_things[n], time_offset);
-            break;
-        case "s":
-            draw_sprite(all_things[n], time_offset);
-            break;
-        case "l":
-            draw_line(all_things[n], time_offset);
-            break;
-        }
-    }
-}
-
-function animate() {
-
-    if (ws_frames > 0) {
-        total_draws += 1;
-    }
-
-    if (total_draws % 10 === 0) {
-        document.getElementById("ws_frames").innerHTML = ws_frames;
-        document.getElementById("total_draws").innerHTML = total_draws;
-    }
-
-    draw();
-    requestAnimationFrame(animate);
-}
-
-function display_debug_message(s) {
-    document.getElementById("debug_msg").innerHTML = "--- " + s;
-}
-
-// Sound from Thomas Sturm: http://www.storiesinflight.com/html5/audio.html
-
-function init_sound() {
-    var a;
-    for (a = 0; a < channel_max; a += 1) {
-        audiochannels[a] = {channel: new Audio(), finished: -1};
-    }
-}
-
-function play_multi_sound(s) {
-    var a;
-    var thistime;
-
-    for (a = 0; a < audiochannels.length; a += 1) {
-        thistime = new Date();
-        if (audiochannels[a].finished < thistime.getTime()) {
-            audiochannels[a].finished = thistime.getTime() + document.getElementById(s).duration * 1000;
-            audiochannels[a].channel.src = document.getElementById(s).src;
-            audiochannels[a].channel.load();
-            audiochannels[a].channel.play();
-            break;
-        }
-    }
-}
-
-// Setup websocket...
-
-var ws = new WebSocket("ws://{{.Server}}{{.WsPath}}");
-
-ws.onopen = function (evt) {
-    requestAnimationFrame(animate);
-};
-
-ws.onmessage = function (evt) {
-
-    var stuff = evt.data.split(" ");
-    var frame_type = stuff[0];
-
-    var n;
-    var len = stuff.length;
-
-    if (frame_type === "v") {
-
-        // Deal with visual frames.................................................................
-
-        all_things.length = 0;
-
-        ws_frames += 1;
-
-        second_last_frame_time = last_frame_time;
-        last_frame_time = Date.now();
-
-        for (n = 1; n < len; n += 1) {
-
-            switch (stuff[n].charAt(0)) {
-
+            switch (all_things[n].type) {
             case "p":
+                draw_point(all_things[n], time_offset);
+                break;
             case "s":
-                parse_point_or_sprite(stuff[n]);
+                draw_sprite(all_things[n], time_offset);
                 break;
             case "l":
-                parse_line(stuff[n]);
+                draw_line(all_things[n], time_offset);
                 break;
             }
         }
+    };
 
-    } else if (frame_type === "a") {
+    that.animate = function () {
 
-        // Deal with audio events..................................................................
-
-        for (n = 1; n < len; n += 1) {
-            play_multi_sound(stuff[n]);
+        if (that.ws_frames > 0) {
+            that.total_draws += 1;
         }
 
-    } else if (frame_type === "d") {
-
-        // Debug messages..........................................................................
-
-        if (evt.data.length > 2) {
-            display_debug_message(evt.data.slice(2));
+        if (that.total_draws % 10 === 0) {
+            document.getElementById("ws_frames").innerHTML = that.ws_frames;
+            document.getElementById("total_draws").innerHTML = that.total_draws;
         }
-    }
-};
 
-// Setup keyboard...
+        that.draw();
+        requestAnimationFrame(that.animate);
+    };
 
-document.addEventListener("keydown", function (evt) {
-    if (evt.key === " ") {
-        ws.send("keydown space");
-    } else {
-        ws.send("keydown " + evt.key);
-    }
-});
+    that.display_debug_message = function (s) {
+        document.getElementById("debug_msg").innerHTML = "--- " + s;
+    };
 
-document.addEventListener("keyup", function (evt) {
-    if (evt.key === " ") {
-        ws.send("keyup space");
-    } else {
-        ws.send("keyup " + evt.key);
-    }
-});
+    // Sound from Thomas Sturm: http://www.storiesinflight.com/html5/audio.html
 
-init_sound();
+    that.init_sound = function () {
+        var a;
+        for (a = 0; a < channel_max; a += 1) {
+            that.audiochannels[a] = {channel: new Audio(), finished: -1};
+        }
+    };
+
+    that.play_multi_sound = function (s) {
+        var a;
+        var thistime;
+
+        for (a = 0; a < that.audiochannels.length; a += 1) {
+            thistime = new Date();
+            if (that.audiochannels[a].finished < thistime.getTime()) {
+                that.audiochannels[a].finished = thistime.getTime() + document.getElementById(s).duration * 1000;
+                that.audiochannels[a].channel.src = document.getElementById(s).src;
+                that.audiochannels[a].channel.load();
+                that.audiochannels[a].channel.play();
+                break;
+            }
+        }
+    };
+
+    that.init_sound();
+
+    // Setup websocket...
+
+    that.ws = new WebSocket("ws://{{.Server}}{{.WsPath}}");
+
+    that.ws.onopen = function (evt) {
+        requestAnimationFrame(that.animate);
+    };
+
+    that.ws.onmessage = function (evt) {
+
+        var stuff = evt.data.split(" ");
+        var frame_type = stuff[0];
+
+        var n;
+        var len = stuff.length;
+
+        if (frame_type === "v") {
+
+            // Deal with visual frames.................................................................
+
+            that.all_things.length = 0;
+            that.ws_frames += 1;
+            that.second_last_frame_time = that.last_frame_time;
+            that.last_frame_time = Date.now();
+
+            // Cache the functions to cut down on indirection (I think).
+            // Might offer a speedup? Who knows...
+
+            var parse_point_or_sprite = that.parse_point_or_sprite
+            var parse_line = that.parse_line
+
+            for (n = 1; n < len; n += 1) {
+
+                switch (stuff[n].charAt(0)) {
+
+                case "p":
+                case "s":
+                    parse_point_or_sprite(stuff[n]);
+                    break;
+                case "l":
+                    parse_line(stuff[n]);
+                    break;
+                }
+            }
+
+        } else if (frame_type === "a") {
+
+            // Deal with audio events..................................................................
+
+            for (n = 1; n < len; n += 1) {
+                that.play_multi_sound(stuff[n]);
+            }
+
+        } else if (frame_type === "d") {
+
+            // Debug messages..........................................................................
+
+            if (evt.data.length > 2) {
+                that.display_debug_message(evt.data.slice(2));
+            }
+        }
+    };
+
+    // Setup keyboard...
+
+    document.addEventListener("keydown", function (evt) {
+        if (evt.key === " ") {
+            that.ws.send("keydown space");
+        } else {
+            that.ws.send("keydown " + evt.key);
+        }
+    });
+
+    document.addEventListener("keyup", function (evt) {
+        if (evt.key === " ") {
+            that.ws.send("keyup space");
+        } else {
+            that.ws.send("keyup " + evt.key);
+        }
+    });
+}
+
+new WsWorldClient();
 
 </script>
 
